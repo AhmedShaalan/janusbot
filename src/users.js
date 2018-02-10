67dfcba006c6f75d6e5e63d15580eea9c0ae865d
Ahmed Shaalan <ahmedshaalan@Youssefs-MacBook-Pro.local>
2018-02-10 19:43:26 +0200

    - load admin users and owners to separatelist when updating users list.

--- src/users.js
+++ src/users.js
@@ -12,34 +12,29 @@ const updateUsersList = data => {
          member.deleted == false
    )
 
-   console.debug(usersList)
+   getAdminUsers()
+   getOwnerUsers()
 }
 
 const getAdminUsers = () => {
    adminUsers = usersList.filter(member => member.is_admin == true)
-
-   console.log(adminUsers)
 }
 
 const getOwnerUsers = () => {
    ownerUsers = usersList.filter(member => member.is_owner == true)
-
-   console.log(ownerUsers)
 }
 
 const getUserObject = userID => {
    let user = usersList.find(user => user.id === userID)
-   console.log(user)
 
    if (user) {
-      console.log(user)
       return user
    } else {
-      console.log(user)
       return false
    }
 }
 
+// this function will return false if the user is deleted OR its a bot.
 const isUserExist = userID => {
    if (getUserObject(userID)) {
       return true
